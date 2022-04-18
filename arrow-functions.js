b6b886a4a75edfd1190d6d58d397ed897f32b808
Kipp AD <kippad@protonmail.com>
2022-04-18 11:37:46 +0000

    Add arrow function examples

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,22 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a,b) {
+    return a + b
+}
 
 // Arrow Function With Parameters
 
+const addTwoNumbers2 = (a,b) => {
+    return a + b
+}
+
 // Single Line Arrow Function With Parameters
+const addTwoNumbers3 = (a,b) => a + b
 
 // Implicit Returns
 
+const printMsg = msg => console.log(msg)
+
 // Returning Multiple Lines
+const sum = num.reduce((partialSum, a) => partialSum + a, 0);
